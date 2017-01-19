5673a2affc5c55dbf463e795aa1776e16021de3b
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-01-19 16:29:20 -0500

    Fixed search 'a' elements. Took out print statements from exceptions.

--- assignments/A1/src/pdfCrawl.py
+++ assignments/A1/src/pdfCrawl.py
@@ -6,27 +6,34 @@
 import sys
 from bs4 import BeautifulSoup
 from urllib2 import urlopen, HTTPError, URLError
+from urlparse import urljoin, urlparse
 
 
-def findPdfs(html):
+def findPdfs(html,baseurl):
     """
     Take html string as parameter and parse through links ('a' elements). Print final redirect url and bytes
-    Params: html string to be used by beautiful soup
+    Params: html string to be used by beautiful soup, baseurl which is passed from commandline
     Return: Array of urls that end with pdf files
     """
     pdfs = []
     soup = BeautifulSoup(html, 'html.parser')
-    for link in soup.find_all('a'):
+    for link in soup.find_all('a',href=True):
+
         linkFound = link.get('href')
+        if isAbsolute(linkFound) == False:
+            linkFound = urljoin(baseurl,linkFound)
+
+        # print "LINK FOUND:",linkFound
         resp = request(linkFound)
-        # print linkFound
-        contentType = resp.info().type
-        responseCode = resp.getcode()
-        if 'pdf' in contentType and responseCode == 200:
-            finalURL = resp.geturl()
-            print finalURL
-            print "Bytes: ", len(resp.read()), "\n"
-            pdfs.append(finalURL)
+        if resp is not None:
+            contentType = resp.info().type
+            responseCode = resp.getcode()
+
+            if 'pdf' in contentType and responseCode == 200:
+                finalURL = resp.geturl()
+                print finalURL
+                print "Bytes: ", len(resp.read()), "\n"
+                pdfs.append(finalURL)
     return pdfs
 
 
@@ -39,19 +46,31 @@ def request(uri):
         response = urlopen(uri)
         return response
     except HTTPError as e:
-        if '\n' in e.reason:
-            errorReason = e.reason.index('\n')
-            print 'Error: ', e.reason[:errorReason]
-        else:
-            print 'Error: File not found'
+        pass
     except URLError as e:
-        print 'We failed to reach a server.'
-        print 'Reason: ', e.reason
+        pass
+    except KeyboardInterrupt:
+        exit()
+
+
+def isAbsolute(url):
+    """
+    Taken from stackoverflow post
+    """
+    try:
+        return bool(urlparse(url).netloc)
+    except:
+        return False
 
 
 if len(sys.argv) == 2:
     response = request(sys.argv[1])
-    pdfs = findPdfs(response.read())
+
+    if response is None:
+        print "Initial link can't be bad"
+        exit()
+
+    pdfs = findPdfs(response.read(),response.geturl())
 else:
     print "Usage: python pdfCrawl.py URI"
     exit()
