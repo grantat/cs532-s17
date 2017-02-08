f2874769f6cd48e4c2937452d1393e0a882ce00e
Grant Atkins <gatkins@Grants-MBP.home>
2017-02-07 23:21:11 -0500

    memento count from timeMaps to csv

--- assignments/A2/src/timeMap.py
+++ assignments/A2/src/timeMap.py
@@ -3,6 +3,7 @@
 import requests
 import os
 import json
+import csv
 
 
 def saveOutput(uri,count):
@@ -11,33 +12,46 @@ def saveOutput(uri,count):
         os.makedirs("output")
 
     # Save json received/created to output
+    fields = [uri,count]
     try:
-        with open('output/timeMaps.json', 'a+b', 0) as file:
-            file.write(bytes(uri + ",\n", encoding='utf-8'))
+        with open('output/timeMaps.csv', 'a', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(fields)
     except (IOError, ValueError):
-        with open('output/finalURIs.txt', 'w') as file:
-            file.write(uri + "\n")
-
-
+        with open('output/timeMaps.csv', 'w', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(fields)
 
 
 def countMementos(jsonResp):
-    print()
+    try:
+        data = json.loads(jsonResp)
+        count = len(data["mementos"]["list"])
+        return count
+    except:
+        return 0
 
-# Remove unwanted from finalURIs
+
+
 with open("output/finalURIs.txt", "r") as file:
     lines = file.readlines()
     for num, line in enumerate(lines):
         try:
             timeMapURI = "http://memgator.cs.odu.edu/timemap/json/" + line
             print(timeMapURI)
-            resp = requests.get(timeMapURI, stream=True, timeout=5, allow_redirects=True, headers={
+            resp = requests.get(timeMapURI, stream=True, allow_redirects=True, headers={
                                 'User-Agent': 'Mozilla/5.0'})
 
             if resp.status_code == 200:
                 # count through json arr
                 print(resp.text)
-                print()
+                count = countMementos(resp.text)
+                saveOutput(line,count)
+                if count > 2:
+                    exit()
+            else:
+                count = 0
+                saveOutput(line,count)
 
         except KeyboardInterrupt:
             print()
