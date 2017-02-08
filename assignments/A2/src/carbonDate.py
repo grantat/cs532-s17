7682003812f9ec70fb20226eeaeb7a332028be16
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-02-08 15:53:07 -0500

    setup of python program for carbon date

---
+++ assignments/A2/src/carbonDate.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+import json
+import csv
+import datetime
+
+def saveOutput(uri,count):
+
+    if not os.path.exists("output"):
+        os.makedirs("output")
+
+    # Save json received/created to output
+    fields = [uri,count]
+    try:
+        with open('output/carbonDate.csv', 'a', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(fields)
+    except (IOError, ValueError):
+        with open('output/carbonDate.csv', 'w', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(fields)
+
+
+def countMementos(jsonResp):
+    try:
+        data = json.loads(jsonResp)
+        count = len(data["mementos"]["list"])
+        return count
+    except:
+        return 0
+
+
+with open("output/finalURIs.txt", "r") as file:
+    lines = file.readlines()
+    for num, line in enumerate(lines):
+        try:
+            timeMapURI = "http://http://localhost:8888/cd?url=" + line
+            print(timeMapURI)
+            resp = requests.get(timeMapURI, stream=True, allow_redirects=True, headers={
+                                'User-Agent': 'Mozilla/5.0'})
+
+            if resp.status_code == 200:
+                # count through json arr
+                print(resp.text)
+                count = countMementos(resp.text)
+                saveOutput(line,count)
+            else:
+                count = 0
+                saveOutput(line,count)
+
+        except KeyboardInterrupt:
+            print()
+            exit()
+        except:
+            pass
