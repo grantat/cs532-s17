729d57f5b2b10fde666fe572331b4ca821b7c292
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-02-09 14:10:13 -0500

    LaTeX compiled, R charts added

--- assignments/A2/src/carbonDateMementos.R
+++ assignments/A2/src/carbonDateMementos.R
@@ -3,5 +3,5 @@ setwd(getwd())
 mementoDays <- read.table(header = FALSE,sep = ",",'output/carbonDateMerged.csv')
 # filtered set w/ atleast 1 memento
 withMementos <- subset(mementoDays, mementoDays$V2 > 0)
-emptyDate <- subset(mementoDays, mementoDays$V3 != "")
-plot(withMementos$V3,withMementos$V2,ylab="Number of Mementos",xlab="Age in Days",main="Scatter Plot for Days vs. Mementos",las=1)
+emptyDate <- subset(mementoDays, is.na(mementoDays$V3))
+plot(withMementos$V3,withMementos$V2,ylab="Number of Mementos",xlab="Age in Days",main="Scatter Plot for Days vs. Mementos")
