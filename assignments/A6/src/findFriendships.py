6fa962319a5052c2fafb8775aa00e435f3fece95
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-03-21 18:29:08 -0400

    Findfriendships code completed. Final commit before long run

--- assignments/A6/src/findFriendships.py
+++ assignments/A6/src/findFriendships.py
@@ -12,13 +12,13 @@ consumer_secret = "EuFivjFeWCBmG205shXMjTPb0u56wTXJgRDRhqaWPRQU1CxYjW"
 
 def minimizeConnections():
     '''
-    Meant to minimize the the connection calls required to determine
+    Meant to minimize the number of connection calls required to determine
     friendship. Creates a dictionary of 100 users mapped to other users
     who aren't already keys.
     For each Key the formula for number of connections to make:
     for 1 in total:
-        Users_to_check = total - iteration_count 
-        total += Users_to_check
+        pairs_to_check = total - iteration_count 
+        total += pairs_to_check
 
     New total = 5151, vs 10100 to check
     '''
@@ -52,29 +52,34 @@ def minimizeConnections():
 
 def findFriendships(api,data):
     # Will take considerable time
-    for i in data:
-        for j in data:
-            
-            if i != j:
-                try:
-                    print(i,"vs",j)
-                    sf = api.show_friendship(source_screen_name=i,target_screen_name=j)
-                    fDict["isFollowing"] = sf[0].following
-                    fDict["isFollowedBy"] = sf[0].followed_by
-                    fDict["source"] = i
-                    fDict["target"] = j
-                except:
-                    print("Failed to connect friendship for: ",i,"and",j)
-                    isFollowing = ""
-                    isFollowedBy = ""
+    for key,val in data.items():
+        for i in val:
+            tempDict = {}
+            try:
+                print(key,"vs",i)
+                sf = api.show_friendship(source_screen_name=key,target_screen_name=i)
+                tempDict["isFollowing"] = sf[0].following
+                tempDict["isFollowedBy"] = sf[0].followed_by
+                tempDict["source"] = key
+                tempDict["target"] = i
+                writeCSV(tempDict,"data/friendships.csv")
+            except KeyboardInterrupt:
+                print()
+                exit()
+            except:
+                print("Failed to connect friendship for: ",key,"and",i)
+                tempDict["isFollowing"] = ""
+                tempDict["isFollowedBy"] = ""
+                tempDict["source"] = key
+                tempDict["target"] = i
+                writeCSV(tempDict,"data/friendships.csv")
 
 
 def writeCSV(data, filename):
     with open(filename, 'a', newline='') as file:
-        for d in data.items():
-            writer = csv.writer(file, delimiter=',')
-            row = [d["name"], d["screen_name"], d["profile_image_url"]]
-            writer.writerow(row)
+        writer = csv.writer(file, delimiter=',')
+        row = [data["source"], data["target"], data["isFollowing"],data["isFollowedBy"]]
+        writer.writerow(row)
 
 
 if __name__ == "__main__":
@@ -85,7 +90,7 @@ if __name__ == "__main__":
     try:
         # findFriendships(api)
         fDict = minimizeConnections()
-        # findFriendships(api,fDict)
+        findFriendships(api,fDict)
         pp(fDict)
         # print(fDict)
     except KeyboardInterrupt:
